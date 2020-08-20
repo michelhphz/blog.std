6c2463976a969e13ec56c74eb7f27c9804973fc3
michelPech <michelhphz@hotmail.com>
2020-08-20 15:53:32 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import firebase from './firebase'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header'
+import Home from './components/Home'
+import './global.css'
+
+class App extends Component{
+  
+  state = {
+    firebaseInitalized: false
+  }
+
+  componentDidMount(){
+    firebase.isInitialized().then(resultado => {
+      this.setState({firebaseInitalized: resultado})
+    })
+  }
+
+  render(){
+    return this.state.firebaseInitalized !== false ? (
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+            <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    ) : (
+      <h1>Carregando...</h1>
+    )
+  }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,43 @@
+import app from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+
+    let firebaseConfig = {
+        apiKey: "AIzaSyAAcTEXd7jKbmVrmr2viGE85XC2S7eANHs",
+        authDomain: "blogstd-2b709.firebaseapp.com",
+        databaseURL: "https://blogstd-2b709.firebaseio.com",
+        projectId: "blogstd-2b709",
+        storageBucket: "blogstd-2b709.appspot.com",
+        messagingSenderId: "628502824662",
+        appId: "1:628502824662:web:15e8d220caa5fe2d11ab77"
+      } 
+
+class Firebase{
+    constructor(){
+        app.initializeApp(firebaseConfig)
+
+        this.app = app.database();
+    }
+
+    login(email, password){
+        return app.auth().signInWithEmailAndPassword(email, password)
+    }
+
+    async register(nome, email, password){
+        await app.auth().createUserWithEmailAndPassword(email, password)
+
+        const uid = app.auth().currentUser.uid;
+
+        return app.database().ref('usuarios').child(uid).set({
+            nome: nome
+        })
+    }
+
+    isInitialized(){
+        return new Promise(resolve => {
+            app.auth().onAuthStateChanged(resolve)
+        })
+    }
+}
+
+export default new Firebase()
